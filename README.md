b699e1e4dc00e26b02734c0c8d6d6d8fe1aa32f7
DenisGuiraudet <guiraudet@live.fr>
2019-11-27 17:30:44 +0100

    Minimal server Dockerized

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # NDI TEMPLATE - Server Node
 
+## Local Development
+
+To install :
+```
+npm install
+```
+
+To start :
+```
+npm start
+```
+
+### Start as Docker
+Note that it won't watch file changes, used for front development or hosting.
+
+To start :
+```
+sudo ./scripts/start_docker.sh
+```
+
 ## Dependencies
 
 - dotenv : https://www.npmjs.com/package/dotenv 
@@ -9,4 +29,6 @@
 ### Dev dependencies
 
 - babel : https://babeljs.io/
+- concurrently : https://www.npmjs.com/package/concurrently
 - nodemon : https://nodemon.io/
+- webpack : https://webpack.js.org/
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,29 @@
 var path = require('path');
+var nodeExternals = require('webpack-node-externals');
 
 var config = {
   target: "node",
+  externals: [nodeExternals()],
   entry: './server.js',
   output: {
-      path: path.resolve(__dirname, 'build'),
-      filename: 'app.bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.bundle.js',
   },
   module: {
-      rules: [{
-          test: /\.js?$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          options: {
-              presets: [
-                  [
-                      "@babel/preset-env",
-                      {
-                          targets: {
-                              node: "10"
-                          }
-                      }
-                  ]
-              ]
-          }
-      }]
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
+      }
+    }]
   },
   resolve: {
     modules: [
       path.resolve('./src'),
       path.resolve('./node_modules')
     ]
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'build'),
-    watchContentBase: true,
-    port: 3000
   }
 };
 
